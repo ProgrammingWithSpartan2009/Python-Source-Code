70a1375f45945a9b5d36409493583917e39709bd
ProgrammingWithSpartan2009 <81898840+ProgrammingWithSpartan2009@users.noreply.github.com>
2021-04-05 17:37:46 +0530

    Create Tut3.py

---
+++ Tut3.py
@@ -0,0 +1,14 @@
+int, str, floats, booleans
+
+ print(type(9.0))
+
+ print(type("993247328573486754830.0"))
+
+ RiceBox = "Rice"
+
+ print(type(RiceBox))
+
+ Number1 = 23 
+ Number2 = 1 
+ Number3 = Number1 + Number2
+ print(Number3)
